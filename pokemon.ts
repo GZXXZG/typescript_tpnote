45b081a27aa0503c3de5d3499b4e2fcec907302f
Romain <romain.gerard2@univ-rouen.fr>
2023-09-19 10:54:32 +0200

    Ajout des dégâts pour type Feu uniquement

--- pokemon.ts
+++ pokemon.ts
@@ -40,6 +40,23 @@ class Pokemon{
                 }
 
             }
+            // Type Feu
+            if (this.type == "Feu"){
+
+                if(p.type == "Feu"){
+                    p.pointsDeVie -= this.pointsAttaque;
+                    console.log(`Attaque réussie ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
+                }
+                if(p.type == "Eau"){
+                    p.pointsDeVie -= this.pointsAttaque*0.5;
+                    console.log(`Ce n'est pas très efficace ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
+                }
+                if(p.type == "Plante"){
+                    p.pointsDeVie -= this.pointsAttaque*2;
+                    console.log(`C'est très efficace ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
+                }
+
+            }
         }
     }
 
