490c44886fa15ee8eb2f7c95679aa486f37332b1
Romain <romain.gerard2@univ-rouen.fr>
2023-09-19 10:58:29 +0200

    Pokémons KO ne peuvent pas être attaqués

--- pokemon.js
+++ pokemon.js
@@ -12,52 +12,79 @@ var Pokemon = /** @class */ (function () {
             return false;
     };
     Pokemon.prototype.attaquer = function (p) {
-        if (p.pointsDeVie == 0)
-            console.log("Le pokémon visé n'a plus de points de vie");
+        if (p.pointsDeVie <= 0)
+            console.log("".concat(p.nom, " est d\u00E9j\u00E0 KO."));
         else {
             // Type Eau
             if (this.type == "Eau") {
                 if (p.type == "Eau") {
                     p.pointsDeVie -= this.pointsAttaque;
-                    console.log("Attaque r\u00E9ussie ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
+                    if (p.pointsDeVie <= 0)
+                        console.log("Bien jou\u00E9 ! ".concat(p.nom, " est KO."));
+                    else
+                        console.log("Attaque r\u00E9ussie ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
                 }
                 if (p.type == "Plante") {
                     p.pointsDeVie -= this.pointsAttaque * 0.5;
-                    console.log("Ce n'est pas tr\u00E8s efficace ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
+                    if (p.pointsDeVie <= 0)
+                        console.log("Bien jou\u00E9 ! ".concat(p.nom, " est KO."));
+                    else
+                        console.log("Ce n'est pas tr\u00E8s efficace ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
                 }
                 if (p.type == "Feu") {
                     p.pointsDeVie -= this.pointsAttaque * 2;
-                    console.log("C'est tr\u00E8s efficace ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
+                    if (p.pointsDeVie <= 0)
+                        console.log("Bien jou\u00E9 ! ".concat(p.nom, " est KO."));
+                    else
+                        console.log("C'est tr\u00E8s efficace ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
                 }
             }
             // Type Feu
             if (this.type == "Feu") {
                 if (p.type == "Feu") {
                     p.pointsDeVie -= this.pointsAttaque;
-                    console.log("Attaque r\u00E9ussie ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
+                    if (p.pointsDeVie <= 0)
+                        console.log("Bien jou\u00E9 ! ".concat(p.nom, " est KO."));
+                    else
+                        console.log("Attaque r\u00E9ussie ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
                 }
                 if (p.type == "Eau") {
                     p.pointsDeVie -= this.pointsAttaque * 0.5;
-                    console.log("Ce n'est pas tr\u00E8s efficace ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
+                    if (p.pointsDeVie <= 0)
+                        console.log("Bien jou\u00E9 ! ".concat(p.nom, " est KO."));
+                    else
+                        console.log("Ce n'est pas tr\u00E8s efficace ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
                 }
                 if (p.type == "Plante") {
                     p.pointsDeVie -= this.pointsAttaque * 2;
-                    console.log("C'est tr\u00E8s efficace ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
+                    if (p.pointsDeVie <= 0)
+                        console.log("Bien jou\u00E9 ! ".concat(p.nom, " est KO."));
+                    else
+                        console.log("C'est tr\u00E8s efficace ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
                 }
             }
             // Type Plante
             if (this.type == "Plante") {
                 if (p.type == "Plante") {
                     p.pointsDeVie -= this.pointsAttaque;
-                    console.log("Attaque r\u00E9ussie ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
+                    if (p.pointsDeVie <= 0)
+                        console.log("Bien jou\u00E9 ! ".concat(p.nom, " est KO."));
+                    else
+                        console.log("Attaque r\u00E9ussie ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
                 }
                 if (p.type == "Feu") {
                     p.pointsDeVie -= this.pointsAttaque * 0.5;
-                    console.log("Ce n'est pas tr\u00E8s efficace ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
+                    if (p.pointsDeVie <= 0)
+                        console.log("Bien jou\u00E9 ! ".concat(p.nom, " est KO."));
+                    else
+                        console.log("Ce n'est pas tr\u00E8s efficace ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
                 }
                 if (p.type == "Eau") {
                     p.pointsDeVie -= this.pointsAttaque * 2;
-                    console.log("C'est tr\u00E8s efficace ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
+                    if (p.pointsDeVie <= 0)
+                        console.log("Bien jou\u00E9 ! ".concat(p.nom, " est KO."));
+                    else
+                        console.log("C'est tr\u00E8s efficace ! ".concat(p.nom, " n'a plus que ").concat(p.pointsDeVie, " points de vie !"));
                 }
             }
         }
@@ -78,3 +105,7 @@ carapuce.attaquer(salameche);
 carapuce.attaquer(bulbizarre);
 bulbizarre.attaquer(carapuce);
 bulbizarre.attaquer(salameche);
+carapuce.attaquer(salameche);
+carapuce.attaquer(salameche);
+carapuce.attaquer(salameche);
+carapuce.attaquer(salameche);
--- pokemon.ts
+++ pokemon.ts
@@ -20,23 +20,32 @@ class Pokemon{
     }
 
     attaquer(p: Pokemon): void {
-        if (p.pointsDeVie == 0)
-            console.log("Le pokémon visé n'a plus de points de vie");
+        if (p.pointsDeVie <= 0)
+            console.log(`${p.nom} est déjà KO.`);
         else{
             // Type Eau
             if (this.type == "Eau"){
 
                 if(p.type == "Eau"){
                     p.pointsDeVie -= this.pointsAttaque;
-                    console.log(`Attaque réussie ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
+                    if (p.pointsDeVie <= 0)
+                        console.log(`Bien joué ! ${p.nom} est KO.`);
+                    else
+                        console.log(`Attaque réussie ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
                 }
                 if(p.type == "Plante"){
                     p.pointsDeVie -= this.pointsAttaque*0.5;
-                    console.log(`Ce n'est pas très efficace ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
+                    if (p.pointsDeVie <= 0)
+                        console.log(`Bien joué ! ${p.nom} est KO.`);
+                    else
+                        console.log(`Ce n'est pas très efficace ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
                 }
                 if(p.type == "Feu"){
                     p.pointsDeVie -= this.pointsAttaque*2;
-                    console.log(`C'est très efficace ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
+                    if (p.pointsDeVie <= 0)
+                        console.log(`Bien joué ! ${p.nom} est KO.`);
+                    else
+                        console.log(`C'est très efficace ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
                 }
 
             }
@@ -45,15 +54,24 @@ class Pokemon{
 
                 if(p.type == "Feu"){
                     p.pointsDeVie -= this.pointsAttaque;
-                    console.log(`Attaque réussie ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
+                    if (p.pointsDeVie <= 0)
+                        console.log(`Bien joué ! ${p.nom} est KO.`);
+                    else
+                        console.log(`Attaque réussie ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
                 }
                 if(p.type == "Eau"){
                     p.pointsDeVie -= this.pointsAttaque*0.5;
-                    console.log(`Ce n'est pas très efficace ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
+                    if (p.pointsDeVie <= 0)
+                        console.log(`Bien joué ! ${p.nom} est KO.`);
+                    else
+                        console.log(`Ce n'est pas très efficace ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
                 }
                 if(p.type == "Plante"){
                     p.pointsDeVie -= this.pointsAttaque*2;
-                    console.log(`C'est très efficace ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
+                    if (p.pointsDeVie <= 0)
+                        console.log(`Bien joué ! ${p.nom} est KO.`);
+                    else
+                        console.log(`C'est très efficace ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
                 }
 
             }
@@ -62,15 +80,24 @@ class Pokemon{
 
                 if(p.type == "Plante"){
                     p.pointsDeVie -= this.pointsAttaque;
-                    console.log(`Attaque réussie ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
+                    if (p.pointsDeVie <= 0)
+                        console.log(`Bien joué ! ${p.nom} est KO.`);
+                    else
+                        console.log(`Attaque réussie ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
                 }
                 if(p.type == "Feu"){
                     p.pointsDeVie -= this.pointsAttaque*0.5;
-                    console.log(`Ce n'est pas très efficace ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
+                    if (p.pointsDeVie <= 0)
+                        console.log(`Bien joué ! ${p.nom} est KO.`);
+                    else
+                        console.log(`Ce n'est pas très efficace ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
                 }
                 if(p.type == "Eau"){
                     p.pointsDeVie -= this.pointsAttaque*2;
-                    console.log(`C'est très efficace ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
+                    if (p.pointsDeVie <= 0)
+                        console.log(`Bien joué ! ${p.nom} est KO.`);
+                    else
+                        console.log(`C'est très efficace ! ${p.nom} n'a plus que ${p.pointsDeVie} points de vie !`);
                 }
 
             }
@@ -95,3 +122,9 @@ carapuce.attaquer(bulbizarre);
 
 bulbizarre.attaquer(carapuce);
 bulbizarre.attaquer(salameche);
+
+
+carapuce.attaquer(salameche);
+carapuce.attaquer(salameche);
+carapuce.attaquer(salameche);
+carapuce.attaquer(salameche);
